ae8172a1326fb3ee1f8d03b209c9f38faaf93df6
patrick nahu <ask.us@diabolicaljewellery.com>
2025-04-14 20:43:30 +1200

    Resolved metadata export issue, fixed ESLint warnings, and optimized images with <Image />

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,15 +7,10 @@ import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Diabolical Jewellery",
-  description: "Luxury jewellery with attitude.",
-};
-
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
